a42feb70ee49e022db41b3bb5bda6f5755f2db64
agungadi <agungadihack@gmail.com>
2021-04-25 14:28:50 +0700

    Praktikum 9: ORM dengan relasi

---
+++ app/Models/Mahasiswa.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Mahasiswa extends Model
+{
+    protected $table="mahasiswas";
+
+    public $timestamps= false;
+    protected $primaryKey = 'Nim';
+
+    protected $fillable = [
+        'Nim',
+        'Email',
+        'Nama',
+        'TglLahir',
+        'Kelas',
+        'Jurusan',
+        'No_Handphone',
+        ];
+
+        public function kelas(){
+            return $this ->belongsTo(Kelas::class);
+        }
+}
